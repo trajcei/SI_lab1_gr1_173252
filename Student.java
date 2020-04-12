d74fd84b2f0d01c9f353084db9639da04900e8a6
unknown <trajceiftimov1@gmail.com>
2020-04-12 19:47:16 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -3,15 +3,41 @@ class Student {
 	String firstName;
 	String lastName;
 
+Student(){}
+
+List<Integer> labPoints = new ArrayList<Integer>() {{ } }; 
+
 	//TODO constructor
 
 	//TODO seters & getters
 
 	public double getAverage() {
 		//TODO
+	return ;
 	}
 
 	public boolean hasSignature() {
 		//TODO
+	return ;
 	}
+}
+
+class Course {
+List<String> Studenti = new ArrayList<String>() {{ } };
+
+Course(){}
+
+public dodadiStudenti() {
+//asd
+	return ;
+	}
+
+public izbrisiStudenti() {
+//asd
+	return ;
+	}
+
+public static void display(){
+System.out.println();
+}
 }
